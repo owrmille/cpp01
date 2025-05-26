8cd97c3f154206bb54b2d2918951a8178893f881
owrmille <iasemin.topchu@gmail.com>
2025-05-26 15:03:07 +0200

    Add ex05

---
+++ ex05/main.cpp
@@ -0,0 +1,17 @@
+#include "Harl.hpp"
+
+int main() { 
+	Harl harl;
+	std::cout << "--------------------------------" << std::endl;
+	harl.complain("DEBUG");
+	std::cout << "--------------------------------" << std::endl;
+	harl.complain("INFO");
+	std::cout << "--------------------------------" << std::endl;
+	harl.complain("WARNING");
+	std::cout << "--------------------------------" << std::endl;
+	harl.complain("ERROR");
+	std::cout << "--------------------------------" << std::endl;
+	harl.complain("OTHER");
+	std::cout << "--------------------------------" << std::endl;
+	return (0);
+}
